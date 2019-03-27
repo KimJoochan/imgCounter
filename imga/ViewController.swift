8c02d729ddc8beb5b6194208df4dc727cc44ad98
dit06 <dit@imac-5.local>
2019-03-26 23:00:38 -0700

    commit 1

--- imga/ViewController.swift
+++ imga/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var cou: UILabel!
+    var count=1;
+    var check=false;
     override func viewDidLoad() {
         super.viewDidLoad()
+        //이미지 로드하기
+        img.image=UIImage(named: "frame\(count).png");
+        cou.text=String(count);
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func update(_ sender: UIButton) {
+       /* if(check==false){
+            count+=1;
+            img.image=UIImage(named: "frame\(count).png");
+            if(count==5){
+                check = !check;
+            }
+        }else if(check==true){
+            count-=1;
+            img.image=UIImage(named: "frame\(count).png");
+            if(count==1){
+                check = !check;
+            }
+        }*/
+        count+=1;
+        cou.text=String(count);
+        if(count<6){
+            img.image=UIImage(named: "frame\(count).png");
+            if(count==5){
+                count=0;
+            }
+        }
+    }
+    
 }
 
